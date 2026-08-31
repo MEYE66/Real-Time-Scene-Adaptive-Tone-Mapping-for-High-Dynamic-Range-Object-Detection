b478f250610c5cd0d6c6f4b047062ec2e28784d3
MEYE66 <meyebest1998@gmail.com>
2026-08-31 21:53:11 +0800

    Remove obsolete code and utility directories

--- mmdet/datasets/transforms/__init__.py
+++
@@ -1,2 +0,0 @@
-# Created: 2025-01-13  
-# Author: Gongzhe Li
--- visualization/__init__.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-# File: __init__.py.py
-# Description: Numpy helpers for image processing
-# Created: 2024-09-02  
-# Author: Gongzhe Li
